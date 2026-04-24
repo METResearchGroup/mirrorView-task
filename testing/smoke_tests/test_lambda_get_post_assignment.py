585a2c124593be96a44cdf5519d44882781421ee
mark-torres10 <markptorres1@gmail.com>
2026-04-24 09:41:10 -0500

    add pilot analysis + smoke test

---
+++ testing/smoke_tests/test_lambda_get_post_assignment.py
@@ -0,0 +1,19 @@
+"""Smoke tests running against the prod lambda.
+
+Test classes:
+1. Invalid inputs (some combination of invalid inputs)
+    - Invalid prolificId
+    - Invalid partyGroup
+    - Invalid studyId
+    - Invalid studyIterationId
+2. Happy flow (typical valid flow)
+"""
+
+LAMBDA_URL = "" # can get these from awscli dynamically probably.
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
